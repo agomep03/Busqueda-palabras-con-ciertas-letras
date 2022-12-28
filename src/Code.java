1a086bfe638ff006200914d57ae1a8e38712a66c
cperes06 <cperes06@estudiantes.unileon.es>
2022-12-28 18:45:03 +0100

    Code.java

--- src/Code.java
+++ src/Code.java
@@ -1,4 +1,37 @@
-package src;
-public static void main(String[] args){
-System.out.println("Hello world");
+import java.util.*;
+
+public class Code{
+	String letters = "Alo";
+	boolean[] Used = new boolean[letters.length()];
+	String ActualWord;
+	int Number = letters.length();
+	String Words;
+	public String DictionarySearch(){
+		
+		for (int i = 0; i <= letters.length(); i++) {
+			Used[i] = false;
+		}
+		
+		for (int i = 0; i <= letters.length(); i++) {
+			DictionarySearchRec(Used[i], "", i);
+		}
+		
+		return "";
+	}
+	
+	public String DictionarySearchRec(Used[], ActualWord, Number) {
+		for (int i = 0; i<= letters.length(); i++) {
+			if (Used[i] == false) {
+				NewWord = ActualWord + letters[i];
+				if ((Number-1) =0 ) {
+					if (Dictionary.contains(NewWord)) {
+						Words += NewWord;
+					}
+				}
+			}
+			Used[i] = True;
+			DictionarySearchRec(Letters, Used, NewWord, Number-1);
+			Used[i] = False;
+		}
+	}
 }
