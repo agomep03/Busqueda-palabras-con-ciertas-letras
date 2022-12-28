68d7f2be7e846b5aa4631ab1eb98e2182c7d1819
cperes06 <cperes06@estudiantes.unileon.es>
2022-12-28 18:54:30 +0100

    Code.java

--- src/Code.java
+++ src/Code.java
@@ -1,37 +1,44 @@
+package src;
+
 import java.util.*;
 
 public class Code{
-	String letters = "Alo";
-	boolean[] Used = new boolean[letters.length()];
-	String ActualWord;
-	int Number = letters.length();
-	String Words;
-	public String DictionarySearch(){
+	public String DictionarySearch(String letters){
+		String Words = "";
+		private Dictionary dictMal;
+		
+		public Code(Dictionary dc) {
+			this.dictMal = dc;
+		}
+		
+		boolean[] Used = new boolean[letters.length()];
+		String ActualWord;
+		int Number = letters.length();
 		
 		for (int i = 0; i <= letters.length(); i++) {
 			Used[i] = false;
 		}
 		
 		for (int i = 0; i <= letters.length(); i++) {
-			DictionarySearchRec(Used[i], "", i);
+			DictionarySearchRec(Used, "", i, letters);
 		}
 		
 		return "";
 	}
 	
-	public String DictionarySearchRec(Used[], ActualWord, Number) {
+	public String DictionarySearchRec(boolean[] Used, String ActualWord, int Number, String letters) {
 		for (int i = 0; i<= letters.length(); i++) {
 			if (Used[i] == false) {
-				NewWord = ActualWord + letters[i];
-				if ((Number-1) =0 ) {
+				String NewWord = ActualWord + letters[i];
+				if (Number-1 == 0 ) {
 					if (Dictionary.contains(NewWord)) {
 						Words += NewWord;
 					}
 				}
 			}
-			Used[i] = True;
-			DictionarySearchRec(Letters, Used, NewWord, Number-1);
-			Used[i] = False;
+			Used[i] = true;
+			DictionarySearchRec(letters, Used, NewWord, Number-1);
+			Used[i] = false;
 		}
 	}
 }
